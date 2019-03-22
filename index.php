5164da924a9a334a254c30388fbdd7dd8a3d86ce
pierrelh <lhopital.pierre@gmail.com>
2019-03-22 19:36:21 -0400

    Create page oxyn

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <ul class="txt-list right-list">
               <li><h4>QU'EST-CE QU'OXYN ?</h4></li>
               <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></li>
-              <li class="btn-presentation"><a href="#">EN SAVOIR PLUS</a></li>
+              <li class="btn-presentation"><a href="./pages/oxyn.php">EN SAVOIR PLUS</a></li>
             </ul>
           </div>
         </li>
---
+++ pages/oxyn.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+    <?php include_once($_SERVER['DOCUMENT_ROOT']."/styles/master.css"); ?>
+    <?php include_once($_SERVER['DOCUMENT_ROOT']."/styles/oxynStyle.css"); ?>
+    </style>
+    <title></title>
+    <?php
+      include_once($_SERVER['DOCUMENT_ROOT']."/assets/header.php");
+    ?>
+  </head>
+  <body>
+    <main>
+      <ul class="main-list">
+        <li><h1>QU'EST-CE QU'OXYN ?</h1></li>
+        <li><h2>DECOUVREZ L'ARTISTE QUI VOUS HABITE</h2></li>
+      </ul>
+    </main>
+    <section>
+      <h3>1. INTRODUCTION</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img class="img-presentation" src="../img/presentation-background1.jpeg" alt="">
+      <h3>2. DEVENIR ARTISTE</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
+
+    <?php
+      include_once($_SERVER['DOCUMENT_ROOT']."/assets/footer.php");
+    ?>
+  </body>
+</html>
